b342896ab9e6153b8f25c0269aa0dbfec826501e
Niharika237 <battuniharika3@gmail.com>
2025-03-05 13:38:07 +0530

    c

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import greet from "calc.js"
+
+import greet from "./calc.js"
+import {add} from "./calc.js"
+let result = greet()
+console.log(result)
+// let result = add(4,5)
+// console.log(result)
